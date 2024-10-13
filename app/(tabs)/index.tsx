cd846f1f3f0e73afb0af352895a448eae1bb5eaa
michaelwong3049 <michaelwong3049@gmail.com>
2024-10-13 13:39:38 -0400

    Initalizing Google Maps API functionality using react-native-maps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-3xl">
-        Come here and be awakened
-      </Text>
+    <View>
+        <MapView 
+          provider={ PROVIDER_GOOGLE }
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          zoomEnabled={true}
+          showsUserLocation
+          style={{height: '100%', width: '100%'}} /> 
     </View>
   );
 }
