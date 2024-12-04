0b31c31bbc707228d3d101c2f063262d03147b70
michaelwong3049 <michaelwong3049@gmail.com>
2024-12-04 17:11:04 -0500

    update TransitMode component to pass in mode and update css for GooglePlacesAutocomplete and other css

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,24 @@
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { Text, View, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
 import TransitMode from "@/components/buttons/TransitMode";
 import * as Location from "expo-location";
-import { useState, useEffect } from "react";
 import MapViewDirections from "react-native-maps-directions";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { GooglePlaceDetail, GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { fetchLocationAlways } from '@/utils/utils'
+
+export type transportationMode = "DRIVING" | "WALKING" | "TRANSIT" | "BICYCLING";
 
 export default function HomeScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [destination, setDestination] = useState<GooglePlaceDetail | null>(null);
   const [findDistance, setFindDistance] = useState(false);
   const [travelTime, setTravelTime] = useState<number | null>(null);
-  const [mode, setMode] = useState<"DRIVING" | "WALKING" | "TRANSIT" | "BICYCLING">("DRIVING")
+  const [mode, setMode] = useState<transportationMode>("DRIVING")
+
+  const onModeSet = (mode: transportationMode) => {
+    setMode(mode)
+  }
 
   useEffect(() => {
     (async () => {
@@ -33,13 +39,31 @@ export default function HomeScreen() {
             language: "en",
           }}
           fetchDetails={true}
+          styles={{
+            textInputContainer: {
+              height: 80,
+              backgroundColor: "transparent",
+              margin: 5
+            },
+            textInput: {
+              height: 40,
+              backgroundColor: "#c8c8c8",
+              borderWidth: 1,
+              borderColor: "black",
+              borderRadius: 25,
+            }
+          }}
         />
       </View>
       
       <View>
-        <Text className="mt-14">Current Destination: { destination?.formatted_address }</Text>
-        <Text className="mb-2">Arrival ETA: {Math.floor(travelTime!)}</Text>
-        <TransitMode setMode={setMode}/>
+        <Text className="text-center font-bold underline mt-14">Current Destination: </Text>
+        <Text className="text-center"> { destination?.formatted_address } </Text>
+        <Text className="text-center mb-2">
+          <Text className="underline font-bold">Arrival ETA:</Text>
+          {''} { Math.floor(travelTime!) } minutes
+         </Text>
+        <TransitMode onModeSet={onModeSet} mode={mode}/>
         <TouchableOpacity
           className="flex items-center bg-purple-400 my-2 py-2 rounded-[25px]"
           onPress={() => {
